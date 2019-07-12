ae57f58261ac0581f9009a764ab7e6854576dec3
Dimitrij <dimitrijfajar@yahoo.co.uk>
2019-07-12 10:21:27 +0700

    Initial Commit

---
+++ stances/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  stances
+//
+//  Created by Dimitrij Fajar Satria Dharma on 12/07/19.
+//  Copyright © 2019 Dimitrij Fajar Satria Dharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
